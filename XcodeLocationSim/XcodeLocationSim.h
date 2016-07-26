1837d3525bfca87b34770c441e9153a4c7057dc3
Ivan Yanakiev <ivan.georgiev.yanakiev@gmail.com>
2016-07-26 23:10:39 +0300

    Initial Commit

---
+++ XcodeLocationSim/XcodeLocationSim.h
@@ -0,0 +1,16 @@
+//
+//  XcodeLocationSim.h
+//  XcodeLocationSim
+//
+//  Created by Ivan Yanakiev on 7/26/16.
+//  Copyright © 2016 Kriviq. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface XcodeLocationSim : NSObject
+
++ (instancetype)sharedPlugin;
+
+@property (nonatomic, strong, readonly) NSBundle* bundle;
+@end
